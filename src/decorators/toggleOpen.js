333731ebc3613520d8a59a7ed9d894be1f120af4
Roman <kaboom.ua@gmail.com>
2017-05-11 11:29:50 +0300

    Update toggleOpen.js

--- src/decorators/toggleOpen.js
+++ src/decorators/toggleOpen.js
@@ -1,11 +1,13 @@
 //HOC === Higher Order Component === decorator
 import React, {Component as ReactComponent} from 'react'
 
+//лучше сделать 2 отдельных декоратора
 export default (OriginalComponent) => class DecoratedComponent extends ReactComponent {
     constructor(props) {
         super()
         this.state = {
             isOpen: false,
+        //Не привязывайся к названиям сущностей, вся суть декораторов в универсальности. Сделай openItemId
             openArticleId: null
         }
     }
